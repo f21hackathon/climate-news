0e066aa67f57b67ef79d9e8fe6a39ba1199ea756
h010dev <mohamed.n.al.hussein@gmail.com>
2021-09-17 18:02:33 -0400

    Move API call to backend
    NewsAPI does not work if calls made from browser.

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 const PORT = 5000;
+
+var funcs = require("./models/model.js");
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -22,3 +25,29 @@ app.use((err, req, res, next) => {
 app.listen(process.env.PORT || PORT, () => {
 	console.log(`Server is listening on port: ${PORT}`);
 });
+
+const country = "Mexico";
+
+async function getNewsArticles(country) {
+	const NEWS_API_KEY = "3a09f01bf6174499b438bfaa14eea1f5";
+	const URL = `https://newsapi.org/v2/everything?q=climate ` +
+		`OR energy OR environment OR global warming OR climate AND ${country} ` + 
+		`change&pageSize=100&from=2021&sortBy=relevancy&apiKey=${NEWS_API_KEY}`;
+
+	try {
+		return await axios.get(URL);
+	} catch (err) {
+		process.stdout.write("Failed to retrieve news articles...\n");
+	}
+}
+
+getNewsArticles(country)
+	.then(res => {
+		for (var i=0; i < res.data.articles.length; i++)
+		{
+			funcs.addArticle(country, res.data.articles[i]);
+		}
+	})
+	.catch(err => {
+		console.log("Something went wrong...\n", err);
+	});
