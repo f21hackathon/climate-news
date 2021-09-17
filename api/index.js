e357de92043c642c9a1b9539e90aa15abf2969f6
h010dev <mohamed.n.al.hussein@gmail.com>
2021-09-17 15:07:38 -0400

    Add CORS support for API

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 app.use("/articles", require("./routes/articles.js"));
 
