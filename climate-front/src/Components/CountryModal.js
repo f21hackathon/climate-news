cdaa730c122809953a965383d1b38b5aa5535e64
h010dev <mohamed.n.al.hussein@gmail.com>
2021-09-18 19:30:48 -0400

    Temporarily remove code for displaying country stats
    Currently breaking app

--- climate-front/src/Components/CountryModal.js
+++ climate-front/src/Components/CountryModal.js
@@ -2,18 +2,17 @@ import React from "react";
 import "./styles/CountryModal.css";
 import CountryStatsList from "./CountryStatsList";
 
-const CountryModal = ({ country, stats }) => {
-	console.log("MODAL: ", country);
-
+const CountryModal = (props) => {
+	console.log("STATS: ", props.stats);
 	return (
 		<div className="country-modal-container">
 			<div className="country-name-container">
-				<img className="flag-img" src={country.image} alt={country.image} />
-				<h3>{country.country}</h3>
+				<img className="flag-img" src={props.country.image} alt={props.country.image} />
+				<h3>{props.country.country}</h3>
 			</div>
 
 			<div className="country-stats">
-				<CountryStatsList countryData={stats}/>
+				<CountryStatsList countryData={props.stats}/>
 			</div>
 
 		</div>
