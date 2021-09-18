09d2c03b80f5f9e06384ffb9e916db4f7dcbda0a
h010dev <mohamed.n.al.hussein@gmail.com>
2021-09-18 15:57:15 -0400

    Change co2 path to local directory
    Testing if this fixes ENOENT error from heroku as it is searching a different path.

--- api/models/climate-data.js
+++ api/models/climate-data.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const jsonPath = path.join(__dirname,  "..", "..", "data", "co2-data.json");
+const jsonPath = path.join(__dirname, "co2-data.json");
 
 const getProperties = async (country) => {
 	let rawdata = fs.readFileSync(jsonPath);
