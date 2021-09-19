0c5cb438286aab6a986e437df2ffd6e10a9a048d
Schow94 <Schow94@gmail.com>
2021-09-19 14:00:53 -0700

    Fix search X bug

--- climate-front/src/Components/Article.js
+++ climate-front/src/Components/Article.js
@@ -6,23 +6,23 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Loading from "./Loading";
-import LinkIcon from '@mui/icons-material/Link';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import LinkIcon from "@mui/icons-material/Link";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function ImgMediaCard({ article }) {
-
 	var raw_article_title = article.title;
 	const ARTICLE_TITLE_LENGTH = 100;
-	var trimmed_article_title = raw_article_title.length > ARTICLE_TITLE_LENGTH ? 
-	                    raw_article_title.substring(0, ARTICLE_TITLE_LENGTH - 1) + "…" : 
-	                    raw_article_title;
-		
-	var raw_author =  article.author ? article.author : 'Author Unknown';
-	const ARTICLE_AUTHOR_LENGTH = 30;
-	var trimmed_author = raw_author.length > ARTICLE_AUTHOR_LENGTH ? 
-	                    raw_author.substring(0, ARTICLE_AUTHOR_LENGTH - 1) + "…" : 
-	                    raw_author;
+	var trimmed_article_title =
+		raw_article_title.length > ARTICLE_TITLE_LENGTH
+			? raw_article_title.substring(0, ARTICLE_TITLE_LENGTH - 1) + "…"
+			: raw_article_title;
 
+	var raw_author = article.author ? article.author : "Author Unknown";
+	const ARTICLE_AUTHOR_LENGTH = 30;
+	var trimmed_author =
+		raw_author.length > ARTICLE_AUTHOR_LENGTH
+			? raw_author.substring(0, ARTICLE_AUTHOR_LENGTH - 1) + "…"
+			: raw_author;
 
 	return (
 		<Card className="article">
@@ -31,7 +31,7 @@ export default function ImgMediaCard({ article }) {
 					component="img"
 					alt={article.urlToImage}
 					height="140"
-					image={article.urlToImage || 'defaultArticleImage.JPG'}
+					image={article.urlToImage || "defaultArticleImage.JPG"}
 				/>
 			</a>
 
@@ -42,26 +42,25 @@ export default function ImgMediaCard({ article }) {
 					component="div"
 					className="article-title">
 					<a className="article-link" href={article.url} target="_blank">
-						{trimmed_article_title} - {trimmed_author}</a> 
-							<CopyToClipboard text={article.url} onCopy={article.url}>
-								<div class="tooltip"><LinkIcon sx={{ ':hover': {boxShadow: 6, color:'success.main'}}}></LinkIcon>
-									<span class="tooltiptext">Copy link</span>
-								</div>
-							</CopyToClipboard>
+						{trimmed_article_title} - {trimmed_author}
+					</a>
+					<CopyToClipboard text={article.url} onCopy={article.url}>
+						<div className="tooltip">
+							<LinkIcon
+								sx={{
+									":hover": { boxShadow: 6, color: "success.main" },
+								}}></LinkIcon>
+							<span className="tooltiptext">Copy link</span>
+						</div>
+					</CopyToClipboard>
 				</Typography>
-				
+
 				<a className="article-link" href={article.url} target="_blank">
 					<Typography variant="body2" color="text.secondary">
 						{article.description}
 					</Typography>
 				</a>
 			</CardContent>
-
-				{/* <CardActions>
-				<Button size="small">Share</Button>
-				<Button size="small">Learn More</Button>
-			</CardActions> */}
-			
 		</Card>
 	);
 }
