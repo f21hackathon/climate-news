8af45430bb47cf4a9be2857701c853ef6f8b6cd4
Asa Holland <asaleholland@gmail.com>
2021-09-19 02:56:57 -0700

    added tooltip and copy function to link icon

--- climate-front/src/Components/Article.js
+++ climate-front/src/Components/Article.js
@@ -7,7 +7,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Loading from "./Loading";
 import LinkIcon from '@mui/icons-material/Link';
-
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export default function ImgMediaCard({ article }) {
 
@@ -20,25 +20,35 @@ export default function ImgMediaCard({ article }) {
 					height="140"
 					image={article.urlToImage}
 				/>
+			</a>
 
-				<CardContent>
-					<Typography
-						gutterBottom
-						variant="h5"
-						component="div"
-						className="article-title">
-						{article.title} - {article.author}  <LinkIcon 
-							sx={{ ':hover': {boxShadow: 6, color:'success.main'}}}></LinkIcon>
-					</Typography>
+			<CardContent>
+				<Typography
+					gutterBottom
+					variant="h5"
+					component="div"
+					className="article-title">
+					<a className="article-link" href={article.url} target="_blank">
+						{article.title} - {article.author}</a> 
+							<CopyToClipboard text={article.url} onCopy={article.url}>
+								<div class="tooltip"><LinkIcon sx={{ ':hover': {boxShadow: 6, color:'success.main'}}}></LinkIcon>
+									<span class="tooltiptext">Copy link</span>
+								</div>
+							</CopyToClipboard>
+				</Typography>
+				
+				<a className="article-link" href={article.url} target="_blank">
 					<Typography variant="body2" color="text.secondary">
 						{article.description}
 					</Typography>
-				</CardContent>
+				</a>
+			</CardContent>
+
 				{/* <CardActions>
 				<Button size="small">Share</Button>
 				<Button size="small">Learn More</Button>
 			</CardActions> */}
-			</a>
+			
 		</Card>
 	);
 }
