1dc2a3cf8d7fab7282b85a3847c3fb9f73db71a4
Matt <mattlam08@gmail.com>
2021-09-19 01:07:15 -0400

    cleaned up code

--- climate-front/src/Components/Navbar.js
+++ climate-front/src/Components/Navbar.js
@@ -9,48 +9,6 @@ import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
 import FreeSoloCreateOption from "./SearchBar";
 
-const Search = styled("div")(({ theme }) => ({
-	position: "relative",
-	borderRadius: theme.shape.borderRadius,
-	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	"&:hover": {
-		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	},
-	marginLeft: 0,
-	width: "100%",
-	[theme.breakpoints.up("sm")]: {
-		marginLeft: theme.spacing(1),
-		width: "auto",
-	},
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: "100%",
-	position: "absolute",
-	pointerEvents: "none",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: "inherit",
-	"& .MuiInputBase-input": {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create("width"),
-		width: "100%",
-		[theme.breakpoints.up("sm")]: {
-			width: "32ch",
-			"&:focus": {
-				width: "40ch",
-			},
-		},
-	},
-}));
-
 const Navbar = ({ countries }) => {
 	// Handles updating state when something is typed in the search bar
 	const [country, setCountry] = useState("");
@@ -77,18 +35,9 @@ const Navbar = ({ countries }) => {
 						sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
 						<u>Climate News</u>
 					</Typography>
-					{/* <Search>
-						<SearchIconWrapper>
-							<SearchIcon />
-						</SearchIconWrapper> */}
-						{/* <StyledInputBase
-							placeholder="Search for a country"
-							inputProps={{ "aria-label": "search" }}
-							onChange={(e) => handleChange(e)}
-							onSubmit={(e) => handleSubmit(e)}
-						/> */}
-						<FreeSoloCreateOption countries={countries} />
-					{/* </Search> */}
+
+					<FreeSoloCreateOption countries={countries} />
+					
 				</Toolbar>
 			</AppBar>
 		</Box>
